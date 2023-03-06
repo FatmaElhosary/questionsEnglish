36e6761b8f20df89f1a8df7ef678248681385a14
Fatma Fathi <f.fathi@gpseducation.com>
2023-03-06 14:59:52 +0200

    make random

--- src/app/components/english-questions/english-questions.component.ts
+++ src/app/components/english-questions/english-questions.component.ts
@@ -2,52 +2,84 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EnglishService } from 'src/app/services/english.service';
 import { DialogAnswerComponent } from '../dialog-answer/dialog-answer.component';
-
+let randomArrayElement = require('@smakss/random-array-element');
 @Component({
   selector: 'app-english-questions',
   templateUrl: './english-questions.component.html',
   styleUrls: ['./english-questions.component.scss'],
 })
 export class EnglishQuestionsComponent implements OnInit {
-  questions = [];
-  private content: string;
-    public query: string="Lorem ipsum";
+  questions: any = [];
+  chooser: any;
+
+  /*   private content: string = '';
+  public query: string = ''; */
   constructor(private _english: EnglishService, public dialog: MatDialog) {
-    this.content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a quam ornare, bibendum ligula a, rhoncus ligula. Etiam aliquet, justo sollicitudin imperdiet luctus, nulla justo sodales mi, sit amet semper nisl velit vel massa. In hac habitasse platea dictumst.";
 
   }
 
   ngOnInit(): void {
     this.getAllParagraphs();
   }
-  public highlight() {
-    if(!this.query) {
-        return this.content;
+
+  /*   public highlight() {
+    if (!this.query) {
+      return this.content;
     }
-    return this.content.replace(new RegExp(this.query, "gi"), match => {
-        return '<span class="highlightText">' + match + '</span>';
+    return this.content.replace(new RegExp(this.query, 'gi'), (match) => {
+      return '<span class="highlightText">' + match + '</span>';
     });
-}
+  } */
   getAllParagraphs() {
     this._english.getAllParagraph().subscribe((data) => {
-      console.log(data.data);
+      // console.log(data.data);
       this.questions = data.data;
+      // this.content = this.questions[0].paragraph;
+      // this.query = this.questions[0].questions[0].answers[0].why[0];
+      // console.log(this.questions[0].questions[0].answers[0].why[0]);
+       this.chooser = randomArrayElement(
+         this.questions[0].questions[0].answers
+       );
     });
   }
-  openDialog(i:number): void {
-   let answers=this.questions.filter(q=>{q})
-   let dialogRef = this.dialog.open(
-     DialogAnswerComponent,
-     {
-       data: { answer: 'ans545' },
-       disableClose: true,
-     },
+  numbers: any[] = [];
+  randomNoRepeats(array: any): any {
+    /*   var copy = array.slice(0);
+    return function () {
+      if (copy.length < 1) {
+        copy = array.slice(0);
+        if (copy.length == 0) {
+          copy = array;
+        }
+      }
+      var index = Math.floor(Math.random() * copy.length);
+      var item = copy[index];
+      copy.splice(index, 1);
+      return item;
+    }; */
+  }
 
-   );
+  openDialog(i: number): void {
+    // console.log(this.generateUniqueRandom(3));
+    //console.log(this.chooser());
 
-    dialogRef.afterClosed().subscribe(result=>{
+    //this.query = this.questions[0].questions[0].answers[0].why[0];
+    let answers = this.questions[i].questions[i].answers;
+
+   // const randomElement = this.chooser();
+
+    //answers[Math.floor(Math.random() * answers.length)];
+    //console.log(randomElement);
+    //console.log(this.questions[i].questions[i].answers);
+    //console.log(randomElement);
+
+    let dialogRef = this.dialog.open(DialogAnswerComponent, {
+      data: { answer: this.chooser().answer },
+      disableClose: true,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog Result:${result}`);
-
-    })
+    });
   }
 }
--- src/app/shared/question/question.component.ts
+++ src/app/shared/question/question.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss']
+  styleUrls: ['./question.component.scss'],
 })
 export class QuestionComponent implements OnInit {
+  @Input() question: any;
+  @Input() quesI:any;
+  @Output() openDialog = new EventEmitter<any>();
+  query: string[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(this.question);
+
+  }
+  doClick(quesI :number, ansI = 0) {
+
+    this.query = this.question.questions[quesI].answers[ansI].why;
+    this.highlight();
+    this.openDialog.emit();
+  }
+
+  public highlight() {
+    if (!this.query) {
+      return this.question.paragraph;
+    }
+    return this.question.paragraph.replace(
+      new RegExp(this.query.join('|'), 'gi'),
+      (match: string) => {
+        return '<span class="highlightText">' + match + '</span>';
+      }
+    );
   }
 
 }
